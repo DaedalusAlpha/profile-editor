f43ccc1bde0413e3ed6174415627f79a7ca147ce
DaedalusAlpha <mattrmcc@gmail.com>
2022-09-29 20:22:28 -0500

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/user.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'profile-editor';
+  title: string = 'Profile Editor';
+  displayEdit: boolean = false;
+  user1: User = {
+    name: 'Matt',
+    age: 40,
+    favoriteColor: 'blue',
+  };
+
+  toggleEdit(): void {
+    this.displayEdit = !this.displayEdit;
+  }
 }
